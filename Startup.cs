01cad3e87fcd030cd08596b9678685e0eb2f184b
ashwini_basavaraju <ashwini_basavaraju@cms.co.in>
2022-09-23 18:36:31 +0530

    commit

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PharmacyMedicineSupplyMicroservice
 {
@@ -26,7 +27,7 @@ namespace PharmacyMedicineSupplyMicroservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CSPharmacyMedicineSupplyManagement.Models.PharmacyMedicineSupplyManagementContext>();
+            services.AddDbContext<CSPharmacyMedicineSupplyManagement.Models.PharmacyMedicineSupplyManagementContext>(c => c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
